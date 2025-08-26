47d19e26150bd88e0aa49710bde0dd3d0e114636
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-27 02:25:14 +0300

    Создаем класс контекста базы данных в папке /Data. Файл /Data/ApplicationDbContext.cs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BMassage.Models;
+
+namespace BMassage.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        // Определяем наборы сущностей
+        public DbSet<Massage> Massages { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
