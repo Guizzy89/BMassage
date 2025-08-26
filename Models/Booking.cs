6c41ca8b73f0112f9a5c8399fa4b3bbf1b1d075b
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-27 02:24:26 +0300

    Создаем две модели: одну для видов массажа, другую для бронирования.

---
+++ Models/Booking.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BMassage.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime SlotDate { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        public bool IsAvailable { get; set; } = true;
+
+        public string ClientName { get; set; } = "";
+
+        public string PhoneNumber { get; set; } = "";
+
+        public string Comment { get; set; } = "";
+    }
+}
