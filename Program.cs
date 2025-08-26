ddf027d286a24fb59ab3e8af2f554591bba6d8e5
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-27 02:20:14 +0300

    Создал папки проекта Views со вложенным Home в котором создал файл Index.cshtml, создал папку Controller и HomeController.cs Поменял код в Program.cs чтобы сразу увидеть вид будущего сайта

--- Program.cs
+++ Program.cs
@@ -1,6 +1,33 @@
+// Приложение начинается с настройки конфигурации
 var builder = WebApplication.CreateBuilder(args);
+
+// Включаем поддержку MVC
+builder.Services.AddControllersWithViews();
+
+// Строим приложение
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    // Если среда не Development, включаем обработку ошибок
+    app.UseExceptionHandler("/Home/Error");
+    // Устанавливаем строгую политику безопасности содержимого
+    app.UseHsts();
+}
 
+// Использовать HTTPS перенаправления
+app.UseHttpsRedirection();
+
+// Поддерживаем статические файлы
+app.UseStaticFiles();
+
+// Запускаем систему маршрутизации
+app.UseRouting();
+
+// Регистрация маршрутов MVC
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Стартуем веб-приложение
 app.Run();
