a9934e223e6a4e6d2dfe99112f186f1207ded646
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-27 02:44:42 +0300

    Этот метод требует скачивания файлов библиотеки Bootstrap. Скачайте сборку: Перейдите на официальный сайт Bootstrap и скачайте готовую сборку. Распакуйте архив: Распакуйте полученный архив. Скопируйте файлы: Скопируйте папку css и js (содержащие bootstrap.min.css и bootstrap.bundle.min.js соответственно) в ваш проект. Подключите в HTML: Вставьте ссылки на локальные файлы в <head> и <body> секции вашего HTML-документа.

---
+++ Views/Massage/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BMassage.Views.Massage
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
