cef38c808336b2df41a2507f73e9d1a53464bcff
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-27 02:26:59 +0300

    в Program.cs обновляем подключение к базе данных

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
 // Приложение начинается с настройки конфигурации
+using BMassage.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Включаем поддержку MVC
 builder.Services.AddControllersWithViews();
+
+// Регистрация контекста базы данных
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Строим приложение
 var app = builder.Build();
@@ -13,6 +20,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // Устанавливаем строгую политику безопасности содержимого
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
 }
 
 // Использовать HTTPS перенаправления
